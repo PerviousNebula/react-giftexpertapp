da8c143f0fadb128a9ee58ee420a56945926541e
PerviousNebula <nevarez.arturo@uabc.edu.mx>
2020-07-17 14:52:02 -0700

    Docs agregado

---
+++ docs/precache-manifest.bed7ae4f43d1b4baaa49bbd7c70b331e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a713c2d765ec4fdcf8b6b571c29a7965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85d97b0328935607cc5a",
+    "url": "/static/css/main.873a62d6.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d97b0328935607cc5a",
+    "url": "/static/js/main.cb363d76.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
